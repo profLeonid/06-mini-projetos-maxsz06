828498d1af2ed4f4a64f53488c1206fb7eaf4bac
MAXWILLIAN ALBINO ZACARIAS DE SANTANA <25203648@CFP127ALUNO.LOCAL>
2026-03-11 11:27:36 -0300

    em andamento

--- 02-calculadora-desconto/app.js
+++ 02-calculadora-desconto/app.js
@@ -1,26 +1,42 @@
 'use strict'
 
+function removerClasses() {
+    const resultado = document.getElementById('resultado')
+    resultado.classList.remove('cincoporcento', 'dezporcento','acimadedez')
+}
+
 function calcularDesconto(){
    
-    const Defpreco = document.getElementById('preco')
-    const Defdesconto =  document.getElementById('desconto')
+    const Defpreco = document.getElementById('preco') // Pegar o objeto
+    const Defdesconto =  document.getElementById('desconto') // Pegar o objeto
+    const ValPreco = Defpreco.value
+    const ValDesconto = Defdesconto.value
 
-    const preco = parseFloat(Defpreco)
-    const desconto = parseFloat(Defdesconto)
+    const preco = Number(ValPreco)
+    const desconto = Number(ValDesconto)
 
-    if (preco === "" || desconto === "") {
+
+    if (Defpreco === "" || Defdesconto === "") {
         alert('Preencha todas as caixas!');
         return false;
     } else {
         let resultado = calcularDesconto(preco, desconto);
-
     }
 
 function calcularDesconto (preco,desconto){
 
+    removerClasses()
     const porcentagem = desconto/100
     const valorComDesconto = preco - (preco*porcentagem)
-        return valorComDesconto
+
+    
+    if(desconto <=5 ){
+        resultado.classList.add('cincoporcento')
+    }else if(desconto >=10){
+        resultado.classList.add('dezporcento')
+    }else if (desconto>10){
+        resultado.classList.add('acimadedez')
+    }
 
 }
 
